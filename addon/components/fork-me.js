b6b491c4d1ee5579ae328c8c8d1c35620a48e71e
Kelly Selden <kellyselden@gmail.com>
2018-12-19 10:19:44 +0000

    refactor positionalParams

--- addon/components/fork-me.js
+++ addon/components/fork-me.js
@@ -1,15 +1,11 @@
 import Component from '@ember/component';
 import layout from '../templates/components/fork-me';
 
-const MyComponent = Component.extend({
+export default Component.extend({
   layout,
 
   cssUrlBase: 'https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.0',
   title: 'Fork me on GitHub'
-});
-
-MyComponent.reopenClass({
+}).reopenClass({
   positionalParams: ['url']
 });
-
-export default MyComponent;
